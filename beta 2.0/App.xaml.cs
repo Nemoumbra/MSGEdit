8e2127b90b95417597fdf578bba880eaf7ea4211
Nemoumbra <62447396+Nemoumbra@users.noreply.github.com>
2022-02-25 20:05:16 +0300

    Changlelog incoming
    Filename in the window name, no crashes, numeration for messages, ect.

---
+++ beta 2.0/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace MSGEdit
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            switch (e.Args.Length) {
+                case 0: {
+                    // Normal startup
+                    MainWindow wnd = new MainWindow();
+                    wnd.Show();
+                    break;
+                }
+                case 1: {
+                    // Expect "filename.txt or filename.pac or filename.msg"
+                    //Console.WriteLine("This is where a new feature will be, for now it's just a placeholder\n");
+                    MessageBox.Show("This is where a new feature will be, for now it's just a placeholder");
+                    Application.Current.Shutdown();
+                    break;
+                }
+                case 2: {
+                    // "-gui filename.txt or filename.pac or filename.msg"
+                    //Console.WriteLine("This is where another new feature will be, for now it's just a placeholder\n");
+                    MessageBox.Show("This is where another new feature will be, for now it's just a placeholder");
+                    Application.Current.Shutdown();
+                    break;
+                }
+                default: {
+                    // Usage from CMD: [-gui] filename.txt or .pac or .msg
+                    //Console.WriteLine("Usage from CMD: [-gui] filename.extension, where extension is either *.txt, *.msg or *.pac\n");
+                    MessageBox.Show("This is where yet another feature will be, for now it's just a placeholder");
+                    Application.Current.Shutdown();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ beta 2.0/Classes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+/// <summary>
+/// 
+/// </summary>
+
+public class PataponMessage : INotifyPropertyChanged {
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void NotifyPropertyChanged(string property_name = "")
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(property_name));
+        }
+    }
+
+    private int index;
+    private string message;
+
+    public int Index
+    {
+        get {
+            return index;
+        }
+        set {
+            index = value;
+            NotifyPropertyChanged("Index");
+        }
+    }
+    public string Message {
+        get {
+            return message;
+        }
+        set {
+            message = value;
+            NotifyPropertyChanged("Message");
+        }
+    }
+    public PataponMessage(string msg) {
+        Message = msg;
+    }
+    //public string To
+        
+}
+/*public class MessageCollection : INotifyPropertyChanged {
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void NotifyPropertyChanged(string property_name = "")
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(property_name));
+        }
+    }
+    public MessageCollection() {
+
+    }
+    private ObservableCollection<PataponMessage> items = new ObservableCollection<PataponMessage>();
+    public ObservableCollection<PataponMessage> Items {
+        get {
+            return items;
+        }
+        set {
+            NotifyPropertyChanged("Items");
+            items = value;
+        }
+    }
+    public void refresh(string property_name = "") {
+        NotifyPropertyChanged(property_name);
+    }
+}*/
