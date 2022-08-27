0d136e3644506e3abc35bebb017a4b1f0c85001b
Nemoumbra <62447396+Nemoumbra@users.noreply.github.com>
2022-08-27 20:10:47 +0300

    Commentary, logic improvements
    Settings reading system completely remade

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -7,8 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.IO;
 
-namespace MSGEdit
-{
+namespace MSGEdit {
     /// <summary>
     /// Логика взаимодействия для App.xaml
     /// </summary>
@@ -16,7 +15,7 @@ namespace MSGEdit
         public Settings settings; // when do I have to initialize them?
         public MSGtoTXT core;
 
-        private bool convert(string path, PataponMessageFormat format) {
+        private bool convert_file(string path, PataponMessageFormat format) {
             if (format == PataponMessageFormat.TXT) {
                 core.LoadTXT(path);
             }
@@ -34,28 +33,21 @@ namespace MSGEdit
             else {
                 core.SaveTXT(path + ".txt");
             }
-
-            if (core.LastError != "") {
-                return false;
-            }
-            return true; // can be rewritten as return core.LastError == "";
+            return core.LastError == "";
         }
-        private bool convert(string path) {
+        private bool convert_file(string path) {
             PataponMessageFormat format = core.LoadAny(path);
             if (!core.isLoaded) {
                 return false;
             }
-            // now format is correct
+
             if (format == PataponMessageFormat.MSG) {
                 core.SaveTXT(path + ".txt");
             }
             else {
                 core.SaveMSG(path + ".msg");
             }
-            if (core.LastError != "") {
-                return false;
-            }
-            return true; // can be rewritten as return core.LastError == "";
+            return core.LastError == "";
         }
 
         void NormalStartup() {
@@ -69,35 +61,61 @@ namespace MSGEdit
             wnd.Show();
         }
 
+        private Dictionary<string, string> get_settings_from_raw(string[] data) {
+            Dictionary<string, string> res = new Dictionary<string, string>();
+            foreach (string line in data) {
+                string[] words = line.Split('=');
+                if (words.Count() == 2) {
+                    res.Add(words[0], words[1]);
+                }
+            }
+            return res;
+        }
+
         private void read_settings_from_file() {
+            // This method is only called if the file exists
+            // It's superfluous to restore default settings if some settings are omitted in the file
             try {
-                StreamReader stream = File.OpenText("settings.txt");
-                string[] data = stream.ReadToEnd().Replace("\r", "").Split('\n');
-                stream.Close();
-                if (data.Length == 3) { // current number of settings
-                    settings.enable_warnings = Convert.ToBoolean(data[0].Split('=')[1]);
-                    settings.check_readme = Convert.ToBoolean(data[1].Split('=')[1]);
-                    settings.deduce_file_type = Convert.ToBoolean(data[2].Split('=')[1]);
+                string[] data;
+                using (StreamReader stream = File.OpenText("settings.txt")) {
+                    data = stream.ReadToEnd().Replace("\r", "").Split('\n');
+                }
+                Dictionary<string, string> dict = get_settings_from_raw(data);
+
+                string value;
+                if (dict.TryGetValue("Enable_warnings", out value)) {
+                    settings.enable_warnings = Convert.ToBoolean(value);
+                }
+                if (dict.TryGetValue("Check_readme", out value)) {
+                    settings.check_readme = Convert.ToBoolean(value);
+                }
+                if (dict.TryGetValue("Deduce_file_type", out value)) {
+                    settings.deduce_file_type = Convert.ToBoolean(value);
+                }
+                if (dict.TryGetValue("Autopaste_from_clipboard", out value)) {
+                    settings.autopaste_from_clipboard = Convert.ToBoolean(value);
                 }
             }
             catch (Exception except) {
-                MessageBox.Show("Error! " + except.Message + "\nUsing default settings");
+                MessageBox.Show("Error! " + except.Message + "\nUsing default settings", "Error!",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
                 settings.restore_default_settings();
             }
         }
         private void make_settings_file() {
             try {
-                StreamWriter stream = File.CreateText("settings.txt");
-                /*"Enable_warnings"=True
-                  "Check_readme"=True
-                  "Deduce_file_type"=False b*/
-                stream.WriteLine("\"Enable_warnings\"=True");
-                stream.WriteLine("\"Check_readme\"=True");
-                stream.Write("\"Deduce_file_type\"=False");
-                stream.Close();
+                /*Enable_warnings=True
+                  Check_readme=True
+                  Deduce_file_type=True */
+                using (StreamWriter stream = File.CreateText("settings.txt")) {
+                    stream.WriteLine("Enable_warnings=" + settings.enable_warnings.ToString());
+                    stream.WriteLine("Check_readme=" + settings.check_readme.ToString());
+                    stream.WriteLine("Deduce_file_type=" + settings.deduce_file_type.ToString());
+                    stream.Write("Autopaste_from_clipboard=" + settings.autopaste_from_clipboard.ToString());
+                }
             }
             catch (Exception except) {
-                MessageBox.Show("Error! " + except.Message + "\nUnable make settings file");
+                MessageBox.Show("Error! " + except.Message + "\nUnable to make settings file");
             }
         }
 
@@ -122,20 +140,26 @@ namespace MSGEdit
                 read_settings_from_file();
             }
             else {
-                if (MessageBox.Show("Settings file not found, should new one be made?", "Prompt", 
+                if (MessageBox.Show("Settings file not found.\nShould the new one be made?", "Prompt", 
                     MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes) {
                     make_settings_file();
                 }
             }
 
             /*
-             * Варианты разрешённых команд:
-             * 1) Стандартный запуск (вне зависимости от настроек) - 0 аргументов.
-             * 2) 1 аргумент - путь к файлу. Здесь происходит либо автоматическое угадывание, либо MessageBox.
-             * 3) 2 аргумента - путь к файлу и -txt или -msg. Здесь игнорируется настройка и происходит запуск согласно типу.
-             * 4) Любое нечётное # аргументов, где 1 - -convert, а остальные - пути к файлам 
-             * с указанием типа -txt или -msg, если deduce = false, иначе без указаний
-             * Правда, возможно -convert file_path, если deduce = true
+             * Allowed commands:
+             * 1) 0 arguments => Standard launch (regardless of settings).
+             * 
+             * 2) 1 argument - the path to the file => Either automatic guessing or MessageBox happens here.
+             * 
+             * 3) 2 arguments - file path and -txt or -msg => This ignores the setting and 
+             * launches the application in accordance with the specified type.
+             * 
+             * 4) If deduce is false, any odd number of arguments, where the first is -convert
+             * and the rest are file paths with type specifications (-txt or -msg)
+             * 
+             * 5) If deduce is true, any number of arguments, where the first one is -convert
+             * and the rest are file paths without type specifications
              */
 
             switch (e.Args.Length) {
@@ -150,20 +174,21 @@ namespace MSGEdit
                     wnd.settings = settings;
                     wnd.core = core;
 
+                    // If not for this part here, we could have used NormalStartup()
                     if (!settings.deduce_file_type) {
 
                         if (MessageBox.Show("Does this file contain MSG?", "Yes/No", MessageBoxButton.YesNo,
                         MessageBoxImage.Question) == MessageBoxResult.Yes) {
-                            // try to access the file and launch as PataponMessageFormat.MSG at workshop state
+                            // Try to access the file and launch as PataponMessageFormat.MSG at workshop state
                             wnd.load_file(e.Args[0], PataponMessageFormat.MSG);
                         }
                         else {
+                            // Try to access the file and launch as PataponMessageFormat.TXT at workshop state
                             wnd.load_file(e.Args[0], PataponMessageFormat.TXT);
-                            // try to access the file and launch as PataponMessageFormat.TXT at workshop state
                         }
                     }
                     else {
-                        // try to access the file, figure out its content and then launch at workshop state
+                        // Try to access the file, figure out its content and then launch at workshop state
                         wnd.load_file(e.Args[0]);
                     }
 
@@ -178,23 +203,36 @@ namespace MSGEdit
 
                     wnd.settings = settings;
                     wnd.core = core;
+                    /*
+                     * 2 argument examples:
+                     * -convert filename.msg
+                     * mission.txt -txt
+                     */
+                    
+                    // Special case:
                     if (e.Args[0] == "-convert" && settings.deduce_file_type) {
-                        if (!convert(e.Args[1])) {
-                            // conversion failure
-                            MessageBox.Show("Conversion of file " + e.Args[1] + " failed!\nError: " + core.LastError, "Error!",
-                                       MessageBoxButton.OK, MessageBoxImage.Error);
+                        if (!convert_file(e.Args[1])) {
+                            // Conversion failure
+                            MessageBox.Show($"Conversion of file {e.Args[1]} failed!\nError: {core.LastError}",
+                                "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
                         }
+                        Application.Current.Shutdown();
                     }
+
                     if (e.Args[1] == "-txt") {
-                        // try to access the file and launch as PataponMessageFormat.TXT at workshop state
+                        // Try to access the file and launch as PataponMessageFormat.TXT at workshop state
                         wnd.load_file(e.Args[0], PataponMessageFormat.TXT);
                     }
-                    if (e.Args[1] == "-msg") {
-                        // try to access the file and launch as PataponMessageFormat.MSG at workshop state
+                    else if (e.Args[1] == "-msg") {
+                        // Try to access the file and launch as PataponMessageFormat.MSG at workshop state
                         wnd.load_file(e.Args[0], PataponMessageFormat.MSG);
                     }
-                    // the branch which was executed has already done everything needed (if it was needed)
-                    // if none of them were executed, nothing changes
+                    else {
+                        MessageBox.Show($"Unknown Patapon message file type {e.Args[1]} (must be either -msg or -txt)",
+                                "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    // The branch which was executed has already done everything needed
+                    // If none of them were executed, nothing changes
                     if (settings.check_readme) {
                         wnd.check_readme();
                     }
@@ -203,26 +241,29 @@ namespace MSGEdit
                 }
                 default: {
                     if (e.Args[0] != "-convert") {
-                        // Shutdown? Or maybe normal startup?
+                        // We ignore command line arguments
                         NormalStartup();
                     }
                     else {
+                        // First argument is -convert
                         if (settings.deduce_file_type) {
-                            // expect any number of file paths
+                            // Expect any number of file paths
                             for (int i = 1; i < e.Args.Length; ++i) {
-                                if (!convert(e.Args[i])) {
-                                    // conversion failure
-                                    MessageBox.Show("Conversion of file " + e.Args[i] + " failed!\nError: " + core.LastError, "Error!",
+                                if (!convert_file(e.Args[i])) {
+                                    // Conversion failure
+                                    MessageBox.Show($"Conversion of file {e.Args[1]} failed!\nError: {core.LastError}", "Error!",
                                         MessageBoxButton.OK, MessageBoxImage.Error);
                                 }
                             }
-                            NormalStartup();
+                            Application.Current.Shutdown();
                         }
                         else {
-                            // expect odd number of args
+                            // Expect odd number of args
                             if (e.Args.Length % 2 == 0) {
                                 // Shutdown? Or maybe normal startup?
-                                NormalStartup();
+                                MessageBox.Show($"File type of {e.Args.Last()} is not specified!", "Error!", 
+                                    MessageBoxButton.OK, MessageBoxImage.Error);
+                                Application.Current.Shutdown();
                                 return;
                             }
 
@@ -232,34 +273,31 @@ namespace MSGEdit
                                 if (e.Args[2 * (i + 1)] == "-txt") {
                                     to_convert.Add(new Tuple<string, PataponMessageFormat>(e.Args[2 * i + 1], PataponMessageFormat.TXT));
                                 }
+                                else if (e.Args[2 * (i + 1)] == "-msg") {
+                                    to_convert.Add(new Tuple<string, PataponMessageFormat>(e.Args[2 * i + 1], PataponMessageFormat.MSG));
+                                }
                                 else {
-                                    if (e.Args[2 * (i + 1)] == "-msg") {
-                                        to_convert.Add(new Tuple<string, PataponMessageFormat>(e.Args[2 * i + 1], PataponMessageFormat.MSG));
-                                    }
-                                    else {
-                                        // this is unacceptable, stop the operation
-                                        MessageBox.Show("Wrong file type specification: " + e.Args[2 * (i + 1)] + 
-                                            "\n(Must be either -msg or -txt\nAborting conversion", 
-                                            "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                                    // This is unacceptable, stop the operation
+                                    MessageBox.Show($"Wrong file type specification: {e.Args[2 * (i + 1)]}" + 
+                                        "\n(Must be either -msg or -txt)\nAborting conversion", 
+                                        "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
 
-                                        NormalStartup();
-                                        return;
-                                    }
+                                    Application.Current.Shutdown();
+                                    return;
                                 }
                             }
+
                             // now process every file one-by-one
                             for (int i = 0; i < count; ++i) {
-                                if (!convert(to_convert[i].Item1, to_convert[i].Item2)) {
-                                    // convertation failure, do something
+                                if (!convert_file(to_convert[i].Item1, to_convert[i].Item2)) {
+                                    // Conversion failure, do something
                                     MessageBox.Show("Conversion of file " + e.Args[i] + " failed!\nError: " + core.LastError, "Error!",
                                         MessageBoxButton.OK, MessageBoxImage.Error);
                                 }
                             }
-                            NormalStartup();
+                            Application.Current.Shutdown();
                         }
                     }
-
-                    //Application.Current.Shutdown();
                     break;
                 }
             }
